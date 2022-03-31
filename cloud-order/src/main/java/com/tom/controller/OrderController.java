cbb9020bb47a0a2826d7c30296050e26d568b53e
weiwensi <17637900215@163.com>
2022-04-01 00:53:18 +0800

    nacos文件配置中心

--- cloud-order/src/main/java/com/tom/controller/OrderController.java
+++ cloud-order/src/main/java/com/tom/controller/OrderController.java
@@ -4,12 +4,15 @@ import com.tom.client.StoreClient;
 import com.tom.dto.Order;
 import com.tom.dto.Store;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class OrderController {
 
     @Autowired
@@ -24,6 +27,20 @@ public class OrderController {
         order.setType("电子产品");
         order.setStore(productStroe);
         return ResponseEntity.ok(order);
+    }
+
+    /**
+     * 测试nacos配置中心
+     * nacos 配置文件名称默认为当前应用名.properties
+     */
+
+
+    @Value("${order.user.name}")
+    private String  name;
+    @GetMapping("testNacos")
+    public ResponseEntity testNacos() {
+        String result = name;
+        return ResponseEntity.ok(result);
     }
 
 }
